19fa980fba79b9f4afd155c704eab4606bc03c07
aaron <aaron.pritzlaff@bjss.com>
2012-02-15 10:31:40 +0000

    results plotting scaled x and y with stubbed (fixed) result

--- Mandelbrot.scala
+++ Mandelbrot.scala
@@ -1,23 +1,40 @@
 object Mandelbrot {
   type N = BigDecimal
+  type Color = Int
 
-  case class Result(pixelCoords : (Int, Int), scaledCoords : (BigDecimal
+  case class Result(pixelCoords : (Int, Int), scaledCoords : (N, N), result : Color)
 
-  def main(args : Array[String]) = {
+  def calculate(x : N, y : N, depth : Int) = {
+     7
+  }
 
-	val fromXY = (0, 0)
-	val toXY = (20,40)
+  def plot(fromXY : (Int, Int), toXY : (Int, Int), depth : Int) : Seq[Result] = {
 
-	val (x1,y1) = fromXY
+val (x1,y1) = fromXY
 	val (x2,y2) = toXY
-	val scaleX = Scale.mapRange(x1)(x2)(-2.5)(1)_
-	val scaleY = Scale.mapRange(y1)(y2)(-1)(1)_
-	val colors = for {coordX <- x1 to x2
+	val scaleX = mapRange(x1)(x2)(-2.5)(1)_
+	val scaleY = mapRange(y1)(y2)(-1)(1)_
+	for {coordX <- x1 to x2
 	     coordY <- y1 to y2
 	     x = scaleX(coordX)
 	     y = scaleY(coordY)
-	     } yield calculate(x,y)
+	     } yield {
+    Result(coordX -> coordY, x -> y,calculate(x,y, depth))
 
+ }
+}
+
+  def main(args : Array[String]) = {
+
+	val fromXY = (5, 5)
+	val toXY = (10,10)
+
+        val depth = 100
+
+	val results = plot(fromXY, toXY, depth)
+
+
+    println(results.mkString("%n".format()))
 
     }
 
