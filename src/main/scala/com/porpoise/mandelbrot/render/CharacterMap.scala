4cc5924adc676de336a64d70d1ed3fe1e28d39e6
Aaron Pritzlaff <aaron@porpoiseltd.com>
2012-02-27 17:53:23 +0000

    monochrome working

--- src/main/scala/com/porpoise/mandelbrot/render/CharacterMap.scala
+++ src/main/scala/com/porpoise/mandelbrot/render/CharacterMap.scala
@@ -5,6 +5,7 @@ import com.porpoise.mandelbrot.Constants.NewLine
 import com.porpoise.mandelbrot.model.Coords
 import com.porpoise.mandelbrot.model.Result
 import com.porpoise.mandelbrot.model.Scale
+import com.porpoise.mandelbrot.Constants.N
 
 class CharacterMap(characters: String, min: Int, max: Int) {
 
@@ -31,16 +32,25 @@ object CharacterMap {
 
   def formatWithCharacters(characters: String = ".-xX*@ ", results: Seq[Result]): String = {
     val charMap = characters.view.toList.map(_.toString)
-    val rangeMap = Scale.mapRange(0)(results.size - 1)(0)(characters.size) _
-    resultsToString(results)(charMap.apply _)
+    val (min, max) = minMax(results)
+    val charIndexForColor = Scale.mapRange(min)(max)(0)(characters.size - 1) _
+    val rangeMap: Color => Int = (c: Color) => (charIndexForColor(c)).toInt
+    val colorToString: Color => String = (c: Color) => {
+      val index: Int = rangeMap(c)
+      charMap(index)
+    }
+    resultsToString(results)(colorToString)
   }
 
   def formatColoredResults(results: Seq[Result]): String = {
-    val colors = results map { _.result }
-    val min = colors.min
-    val max = colors.max
+    val (min, max) = minMax(results)
     val charMap = new CharacterMap(".-xX*@ ", min, max)
     resultsToString(results)(charMap.apply _)
+  }
+
+  private def minMax(results: Seq[Result]) = {
+    val colors = results map { _.result }
+    colors.min -> colors.max
   }
 
   private def resultsToString(results: Seq[Result])(charMap: Color => String): String = {
