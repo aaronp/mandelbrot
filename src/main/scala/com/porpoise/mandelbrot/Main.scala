c573be1c85d39e0fc3187c4ed2f43c15ca75b8b1
Aaron Pritzlaff <aaron@porpoiseltd.com>
2012-03-22 22:10:27 +0000

    usage

--- src/main/scala/com/porpoise/mandelbrot/Main.scala
+++ src/main/scala/com/porpoise/mandelbrot/Main.scala
@@ -33,6 +33,32 @@ import com.porpoise.mandelbrot.io.Files
 object Main {
   import InputCommand._
 
+  def usage = """Usage:
+    | Use the arrow keys to change the percentage the image is translated each frame.
+    |
+    | For example, if you enter left (<-), the x-axis translation will be set to -1%.
+    | Every time the series is rendered, it will be translated to the left by 1%.
+    |
+    | If you enter <- again, then it will be changed to 2%.
+    |
+    | Each input will thus change the translation percentage by 1%.
+    |
+    | The space bar resets the view to the origin.
+    |
+    | The +/- keys will change the zoom by 1%, respectively.
+    |
+    | The 'q' key quits.
+    |
+    | The 'g' key requests the current state.
+    |
+    | The 'r' key resets the translation/scale percentages.
+    |
+    | The 'w' key starts auto-play, where the series will be re-rendered continuously 
+    | using the current translation/scale percentages.
+    |
+    | The 's' key will pause auto-play.
+    """.stripMargin
+
   def readLoop(config: Config)(onMessage: MandelbrotRequest => Boolean) = {
     import config._
 
@@ -48,7 +74,6 @@ object Main {
         case Space => SetAbsoluteViewRequest(size = resolution)
         case Plus => ZoomRequest(-1 * adjustment)
         case Minus => ZoomRequest(adjustment)
-        case Minus => ZoomRequest(-1 * adjustment)
         case Quit => Stop()
         case GetState => GetStateRequest()
         case Reset => SetStateRequest()
@@ -70,6 +95,8 @@ object Main {
       val GetStateResponse(scaledView, resolution, _, _, _) = resultFuture()
       scaledView
     }
+
+    println(usage)
 
     Config.withConfig(System.in) { config =>
       import config._
